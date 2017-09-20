0bc64bbc73bb0d1b86e221c0710e8c214e67466a
Lewis Sparlin <lewis@edatasource.com>
2017-09-20 14:18:45 -0500

    added socket listening at localhost:3000

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 var randomInt = require('random-int')
-var io = require('socket.io-client')()
+var io = require('socket.io-client')
+
+var socket = io.connect('http://localhost:3000')
 
 let bodyEl = document.getElementsByTagName('body')[0]
 let bgStyleTemplate = (color) => `background-color: ${color};`
@@ -11,6 +13,11 @@ const changeColorRandomOnLoad = () => {
 }
 
 const changeColorOnSocketSubscription = () => {
+  socket.on('change-color', data => {
+    console.log('change color', data)
+    bodyEl.setAttribute('style', bgStyleTemplate('#' + data.color))
+  })
 }
 
 changeColorRandomOnLoad()
+changeColorOnSocketSubscription()
