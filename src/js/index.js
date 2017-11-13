111f4bad5a6b5fdca0c86f2c93de0f0165e177cd
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-11-13 14:08:57 -0600

    Added conclusionUrl logic

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import NoSleep from 'my-nosleep'
 let url = ENV.socketUrl || 'http://localhost:3000'
 var socket = io.connect(url)
 var noSleep = new NoSleep()
+var conclusionUrl = '/thanks'
 
 let bgEl = document.getElementById('background-content')
 let contentEl = document.getElementById('content')
@@ -12,7 +13,9 @@ let bgStyleTemplate = (color) => `background-color: ${color};`
 
 axios.get(url + '/settings').then(response => {
   let settings = response.data
+
   document.getElementById('introductoryText').textContent = settings.introductoryText
+  conclusionUrl = settings.conclusionUrl
 })
 
 const changeColorOnSocketSubscription = () => {
@@ -22,12 +25,8 @@ const changeColorOnSocketSubscription = () => {
     }
     bgEl.setAttribute('style', bgStyleTemplate('#' + data.color))
   })
-  socket.on('conclude', data => {
-    if (data && data.location) {
-      location.assign(data.location)
-    } else {
-      location.assign('/thanks')
-    }
+  socket.on('conclude', () => {
+    location.assign(conclusionUrl)
   })
 }
 
