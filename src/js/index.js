64a4115026bbd0db5976f3318e73db198053c873
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-11-15 23:32:42 -0600

    Added function for processing next color on client if necessary

--- src/js/index.js
+++ src/js/index.js
@@ -18,19 +18,33 @@ axios.get(url + '/settings').then(response => {
   conclusionUrl = settings.conclusionUrl
 })
 
-const changeColorOnSocketSubscription = () => {
+var timer
+var currentColor
+const changeBackgroundColor =(colorData) => {
+  if (colorData.color != currentColor) {
+    console.log('setting color ', colorData.color)
+    bgEl.setAttribute('style', bgStyleTemplate('#' + colorData.color))
+  }
+  currentColor = colorData.color
+  clearTimeout(timer)
+  if (colorData.next) {
+    timer = setTimeout(() => changeBackgroundColor(colorData.next), colorData.next.afterDur - 150)
+  }
+}
+
+const subscribeToSocketEvents = () => {
   socket.on('change-color', data => {
     if (contentEl) {
       contentEl.remove()
     }
-    bgEl.setAttribute('style', bgStyleTemplate('#' + data.color))
+    changeBackgroundColor(data)
   })
   socket.on('conclude', () => {
     location.assign(conclusionUrl)
   })
 }
 
-changeColorOnSocketSubscription()
+subscribeToSocketEvents()
 
 var noSleepBtn = document.getElementById('enable-nosleep')
 const enableNoSleep = () => {
